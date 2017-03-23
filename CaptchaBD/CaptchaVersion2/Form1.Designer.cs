080fb3616de93b90196f5856cd3a0841912f0ec3
Jesus Antonio JD <juarez_devora_ja@hotmail.com>
2017-03-23 08:18:01 -0800

    Add files via upload

---
+++ CaptchaBD/CaptchaVersion2/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CaptchaVersion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtCaja = new System.Windows.Forms.TextBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 56);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtCaja
+            // 
+            this.txtCaja.Location = new System.Drawing.Point(12, 102);
+            this.txtCaja.Name = "txtCaja";
+            this.txtCaja.Size = new System.Drawing.Size(157, 20);
+            this.txtCaja.TabIndex = 1;
+            this.txtCaja.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.txtCaja.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Image = ((System.Drawing.Image)(resources.GetObject("btnNext.Image")));
+            this.btnNext.Location = new System.Drawing.Point(244, 74);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(68, 74);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escriba en texto del captcha de arriba";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Espera para que se reproducta el sonido!!!\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 159);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.txtCaja);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CAPTCHA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtCaja;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CaptchaBD/CaptchaVersion2/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CaptchaVersion2
+{
+    public partial class Form1 : Form
+    {
+        //Equipo
+        //Medina Díaz Eduardo
+        //Contreras Acosta Cesar
+        //Ortiz Hernández Rocío Guadalupe
+        //Juárez Devora Jesús Antonio
+        //Rodríguez Vásquez Gerardo
+        //Martínez Rodríguez Francisco Alfonso
+
+        //Se crea una conexion a mysql
+
+        MySqlConnection Cnn = new MySqlConnection();
+        //Declaramos una instancia de objeto random
+        Random r = new Random();
+        //Declaramos un auxiliar para el texto de la caja de texto
+        string aux1 = "";
+        string auxnombre = "", direccion = "";
+        //Declaramos una variable para la posicion
+        int posicion = 0;
+        //Sonidos para si el texto es correcto o incorrecto
+        //Declaramos una instancia de objeto tipo SoundPlayer
+        SoundPlayer simpleSound = new SoundPlayer(@"Goods.wav");
+        SoundPlayer simpleSoundRed = new SoundPlayer(@"Mals.wav");
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            final();
+         
+        }
+
+        private void final()
+        {
+            txtCaja.Text = "";
+            posicion = 0;
+            this.BackColor = Color.FromArgb(224, 224, 224);
+            posicion = r.Next(1, 6);
+            try
+            {
+                //Guardamos la direccion de conexion en una conexion string
+                Cnn.ConnectionString = "Server = localhost; Database = eventos; User Id = root; Password = hola; ";
+                //Abrimos conexion
+                Cnn.Open();
+                //Generamos una intancia de objeto de tipo comando Mysql la cual contiene la consulta
+                MySqlCommand myCmd = new MySqlCommand("select * from pictures where Id='" + posicion + "'");
+                //Para que el comando busque la dirreccion
+                myCmd.Connection = Cnn;
+                //Se crea una instancia de objeto la cual almane el comando para ejecutarlo y regresar un valor
+                //El data reader recupera datos 
+                // El executereader ejecuta el comando y regresa un valor (te regrasa el numero de columnas afectadas)
+                MySqlDataReader myReaader = myCmd.ExecuteReader();
+                //Lee toda la tabla
+                while (myReaader.Read())
+                {
+                    //Sigue leyendo la tabla
+                    myReaader.Read();
+                    //Busca una la columna la convierte en string y la almacena en una variable
+                    auxnombre = myReaader["nombre"].ToString();
+                    //La ruta le cambiamos la direccion de las diagonales y le ponemos dos en vez de uno
+                    direccion = myReaader["imagen"].ToString();
+                }
+                /* Teniendo la direccion, decimos que el pictureBox1 busque la locacion de la imagen
+                Ya que la variable direccion tiene la direccion donde esta la imagen
+                Y con el ImageLocation se busca esa direccion */
+                pictureBox1.ImageLocation = direccion;
+                //Cerramos conexion
+                Cnn.Close();
+            }
+            catch (MySqlException er)
+            {
+                MessageBox.Show("You have an error " + er);
+            }
+        }
+
+        private void done()
+        {
+            aux1 = "";
+            simpleSound.Load();
+            simpleSoundRed.Load();
+            aux1 = txtCaja.Text;
+            if (aux1.Equals(auxnombre) && posicion == 1)
+            {
+                simpleSound.Play();
+                this.BackColor = Color.Green;
+                txtCaja.Text = "";
+                MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                final();
+            }
+
+            else if (aux1.Equals(auxnombre) && posicion == 2)
+            {
+                simpleSound.Play();
+                this.BackColor = Color.Green;
+                txtCaja.Text = "";
+                MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                final();
+            }
+            else if (aux1.Equals(auxnombre) && posicion == 3)
+            {
+                simpleSound.Play();
+                this.BackColor = Color.Green;
+                txtCaja.Text = "";
+                MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                final();
+            }
+            else if (aux1.Equals(auxnombre) && posicion == 4)
+            {
+                simpleSound.Play();
+                this.BackColor = Color.Green;
+                txtCaja.Text = "";
+                MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                final();
+            }
+            else if (aux1.Equals(auxnombre) && posicion == 5 || posicion == 6)
+            {
+                simpleSound.Play();
+                this.BackColor = Color.Green;
+                txtCaja.Text = "";
+                MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                final();
+            }
+            else
+            {
+                simpleSoundRed.Play();
+                this.BackColor = Color.Red;
+                MessageBox.Show("You are a machine!!", "MACHINE", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                final();
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                done();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            final();
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            done();
+        }
+    }
+}
